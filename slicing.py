da623b8af28c4accd108c6f4337b536d6fb4b9e0
Anit Jose <166154249+Anitjose05@users.noreply.github.com>
2025-12-15 09:53:14 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,18 @@
+s="HelloWorld"
+last_five_characters = s[5:]
+print(last_five_characters)  
+
+reverse=s[-1:-11:-1]
+print(reverse)
+
+r2=s[-1::-1]
+print(r2)
+
+data="myprogram.exe"
+print(data[2])
+print(data[-1])
+print(len(data))
+print(data[0:8])    
+print(data[5:9])
+print(data[:9])
+print(len(data)/2)

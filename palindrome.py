988c266be139598aa682a988df4f48af9115571d
Anit Jose <166154249+Anitjose05@users.noreply.github.com>
2025-12-18 09:46:30 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,24 @@
+inputString = input("Enter a string to check whether it is palindrome or not: ")
+inputString = inputString.upper()
+if inputString == inputString[::-1]:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+    
+
+
+
+
+txt="This is a test string.How are you?"
+word=0
+
+
+
+
+print i in [5,4,3,2,1]:
+    print(i)
+print("Blastoff!")
+
+
+
+

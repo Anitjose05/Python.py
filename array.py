6c4a436838e6daf785ee4bac3e2cee499d137ce3
Anit Jose <166154249+Anitjose05@users.noreply.github.com>
2025-12-18 09:45:53 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,23 @@
+friends=['Tess George','Amitha Anil','Thresy Sebastian','Delphy Joseph','Maria Sebastian']     
+
+for friend in friends:
+    print("Happy New Year:", friend)
+print('Done!')
+friends[1]='Niranjana B' 
+for friend in friends:
+    print("Happy New Year:", friend)
+print(len(friends))
+
+
+
+for i in [5, 4, 3, 2, 1]:
+    print(i)
+
+print("Blastoff!")
+txt="This is a test string.How are you?"
+word=0
+for char in txt:
+    if char==' ':
+        word=word+1
+word=word+1
+print("Number of words in the string:", word)
